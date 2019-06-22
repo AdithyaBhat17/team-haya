0148fedeec69036bc90135fecd48e1811bebae18
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-06-22 10:41:28 +0530

    :sparkles: changing background images

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const backgroundChange = () => {
+    const domNode = document.getElementById('hero')
+    for(let i=0; i<12;i++) {
+        setTimeout(() => {
+            domNode.style.backgroundImage = 
+            `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('./assets/backgrounds/${i + 1}.JPG')`
+        }, (i + 1) * 3000)
+    }
+        
+    
+}
+
+backgroundChange()
